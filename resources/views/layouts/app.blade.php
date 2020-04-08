0b64b4ec9a18f2c9efa99d31f3d8fd9cf9e8e5b4
sllem1975 <sllem1802@gmail.com>
2020-04-08 00:20:25 -0400

    Created desing welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'West Island Oldtimers Soccer League') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,43 +18,52 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+  <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+
+    <script>
+        setTimeout(function(){
+            location.reload();
+        },15000); // 5000 milliseconds means 5 seconds.
+      </script>
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm ">
+            <div class="container ">
+                <a class="navbar-brand " href="{{ url('/') }}">
+                    West Island Oldtimers Soccer League
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto ">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            <li class="nav-item ">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li class="nav-item " >
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-right " aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,303 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<div class="container">
+    <section id="match">
+      <div class="row">
+        <div class="box col-12">
+          {{-- <img class="img-fluid bgp1" src="/images/emilio-garcia-AWdCgDDedH0-unsplash.jpg" alt="background soccer league"> --}}
+          <div class="img-title">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+            {{-- <h1 class="text-white display-4 ">West Island Oldtimers</h1>
+            <h1 class="text-white display-4 ">Soccer League</h1> --}}
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="carousel-item active">
+                  <div class="card  mb-3">
+                    <div class="card-header bg-primary text-white ">
+                      <h4> Sun, 04 AUG 2020 16:00 </h4>
+                      <h3>TURF </h3>
+                    </div>
+                    <div class="card-body  ">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <h4 class="cart-title">Phenix </h4>
+                          <h2> <span class="badge badge-primary"> 4 </span></h2>
+                        </div>
+                        <div class="col-md-6">
+                          <h4 class="cart-title">Cuervo</h5>
+                            <h2> <span class="badge badge-danger"> 2 </span></h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-footer bg-primary">
+                      <h4 class="text-white">Dorval Comunite</h4>
+                    </div>
+                  </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="carousel-item">
+                  <div class="card  mb-3">
+                    <div class="card-header bg-primary text-white ">
+                      <h4> Sun, 04 AUG 2020 16:00 </h4>
+                      <h3>GRASS </h3>
+                    </div>
+                    <div class="card-body  ">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <h4 class="cart-title">SVG </h4>
+                          <h2> <span class="badge badge-success"> 2 </span></h2>
+                        </div>
+                        <div class="col-md-6">
+                          <h4 class="cart-title">Dollard</h5>
+                            <h2> <span class="badge badge-success"> 2 </span></h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-footer bg-primary">
+                      <h4 class="text-white">Roxboro 9th Ave.</h4>
+                    </div>
+                  </div>
                 </div>
+
+                <div class="carousel-item">
+                  <div class="card  mb-3">
+                    <div class="card-header bg-primary text-white ">
+                      <h4> Sun, 04 AUG 2020 16:00 </h4>
+                      <h3>FIELD </h3>
+                    </div>
+                    <div class="card-body  ">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <h4 class="cart-title">Hilltoppers </h4>
+                          <h2> <span class="badge badge-danger"> 3 </span></h2>
+                        </div>
+                        <div class="col-md-6">
+                          <h4 class="cart-title">Cuervos</h5>
+                            <h2> <span class="badge badge-primary"> 4 </span></h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-footer bg-primary">
+                      <h4 class="text-white">Dollard Lake Street</h4>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon bg-dark " aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
+
+          </div>
+
         </div>
-    </body>
-</html>
+
+
+      </div>
+    </section>
+    <section id="table" class="bg-light ">
+      <div class="row ">
+        <div class="col-12">
+          <div class="table-responsive ">
+          <table class="table table-hover table-striped table-bordered table-sm ">
+            <thead class="bg-dark text-white">
+              <tr>
+                <th>#</th>
+                <th>TEAM</th>
+                <th>GP</th>
+                <th>W</th>
+                <th>L</th>
+                <th>D</th>
+                <th>GF</th>
+                <th>GA</th>
+                <th>GD</th>
+                <th>PTS</th>
+                <th>Y</th>
+                <th>R</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr >
+                <th scope="row">1</th>
+                <td>Roxboro Rockbottons</td>
+                <td>10</td>
+                <td>8</td>
+                <td>1</td>
+                <td>1</td>
+                <td>37</td>
+                <td>7</td>
+                <td>30</td>
+                <td>25</td>
+                <td>0</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Challengers</td>
+                <td>9</td>
+                <td>7</td>
+                <td>1</td>
+                <td>1</td>
+                <td>39</td>
+                <td>8</td>
+                <td>31</td>
+                <td>22</td>
+                <td>4</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Hilltoppers</td>
+                <td>8</td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>21</td>
+                <td>17</td>
+                <td>4</td>
+                <td>16</td>
+                <td>14</td>
+                <td>4</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Phenix</td>
+                <td>9</td>
+                <td>5</td>
+                <td>4</td>
+                <td>0</td>
+                <td>20</td>
+                <td>20</td>
+                <td>0</td>
+                <td>15</td>
+                <td>13</td>
+                <td>1</td>
+              </tr>
+            </tbody>
+            
+          </table>
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="tab" class="bg-light mt-2 rounded">
+      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Match</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Cards</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Games</a>
+        </li>
+      </ul>
+      <div class="tab-content " id="pills-tabContent">
+        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+          <div class="row">
+            <div class="col-12">
+              <table class="table table-sm">
+                <thead class="bg-dark text-white">
+                  <tr>
+                    <th>#</th>
+                    <th>TEAM</th>
+                    <th>GP</th>
+                    <th>W</th>
+                    <th>L</th>
+                    <th>D</th>
+                    <th>GF</th>
+                    <th>GA</th>
+                    <th>GD</th>
+                    <th>PTS</th>
+                    <th>Y</th>
+                    <th>R</th>
+    
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Roxboro Rockbottons</td>
+                    <td>10</td>
+                    <td>8</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>37</td>
+                    <td>7</td>
+                    <td>30</td>
+                    <td>25</td>
+                    <td>0</td>
+                    <td>1</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Challengers</td>
+                    <td>9</td>
+                    <td>7</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>39</td>
+                    <td>8</td>
+                    <td>31</td>
+                    <td>22</td>
+                    <td>4</td>
+                    <td>1</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Hilltoppers</td>
+                    <td>8</td>
+                    <td>5</td>
+                    <td>2</td>
+                    <td>1</td>
+                    <td>21</td>
+                    <td>17</td>
+                    <td>4</td>
+                    <td>16</td>
+                    <td>14</td>
+                    <td>4</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Phenix</td>
+                    <td>9</td>
+                    <td>5</td>
+                    <td>4</td>
+                    <td>0</td>
+                    <td>20</td>
+                    <td>20</td>
+                    <td>0</td>
+                    <td>15</td>
+                    <td>13</td>
+                    <td>1</td>
+                  </tr>
+                </tbody>
+                
+              </table>
+            </div>
+          </div>  
+        </div>
+        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">text 2</div>
+        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">text 3</div>
+      </div>
+    </section>
+
+  </div>
+@endsection
